dc9664cd67a36d9db450c85fb208dbaaa67ba0f0
ashishr1728 <143636360+ashishr1728@users.noreply.github.com>
2024-08-02 15:10:58 +0530

    Update Problem2.cpp

--- Problem2.cpp
+++ Problem2.cpp
@@ -4,7 +4,10 @@
 using namespace std;
 
 void distribution(vector<int> &apple, vector<int> &arr, double target){
+    // 8. Here we created one variable sum which stors the current sum of weight that each one get.
     int sum = 0;
+
+    // 9. We all the weight in sum and check if the sum is less than or equal to target then add to the vector arr and update the sum variable and put -1 in that index. Otherwise continue.
     for(int i = 0; i < apple.size(); i++){
         if((apple[i] != -1) && (sum + apple[i] <= target)){
             arr.push_back(apple[i]);
@@ -16,13 +19,17 @@ void distribution(vector<int> &apple, vector<int> &arr, double target){
         }
     }
     
+    // 10. Here we print all the apples weight that Ram, Sham and Rahim gets.
     for(int i = 0; i < arr.size(); i++){
         cout<<arr[i]<<" ";
     }
 }
 
 int main(){
+    // 1. Created one vector name apple in which we stored the weight of the apple.
     vector<int> apple;
+
+    // 2. Then play a while loop and get the input from the user utill it gives -1.
     while(1){
         int weight;
         cout<<"Enter apple weight in gram (-1 to stop ) : ";
@@ -37,31 +44,34 @@ int main(){
     }
     
     int n = apple.size();
+    // 3. Then find the total weight of all the apples.
     int weight_sum = 0;
     for(int i = 0; i < n; i++){
         weight_sum += apple[i];
     }
-    
+    // 4. Sort the vector apple in decreasing order.
     sort(apple.rbegin(), apple.rend());
-    vector<int> app = apple;
     
+    // 5. Created 3 another vector in which we stored the apple of Ram, Sham and Rahim.
     vector<int> Ram;
     vector<int> Sham;
     vector<int> Rahim;
     
+    // 6. Find the total weight of the apple for Ram, Sham and Rahim according to their money they paid. 
     double Ram_sum = (weight_sum * 0.5);
     double Sham_sum = (weight_sum * 0.3);
     double Rahim_sum = (weight_sum * 0.2);
     
+    // 7. Call function distribution separately for Ram,Sham and Rahim. In which we pass 3 parameters one is vector apple which stored the weight of all the apples, second one is Vector which stores apple for all three of them seperately and third one is the weight of the apple they get.
     cout<<"Distribution Result :"<<endl;
     cout<<"Ram : ";
-    distribution(app, Ram, Ram_sum);
+    distribution(apple, Ram, Ram_sum);
     cout<<endl;
     cout<<"Sham : ";
-    distribution(app, Sham, Sham_sum);
+    distribution(apple, Sham, Sham_sum);
     cout<<endl;
     cout<<"Rahim : ";
-    distribution(app, Rahim, Rahim_sum);
+    distribution(apple, Rahim, Rahim_sum);
     cout<<endl;
     
     return 0;
